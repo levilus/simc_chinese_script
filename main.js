01ab438f36f71cf11a62b68bea1ebce4af50543f
levilus <139854093+levilus@users.noreply.github.com>
2024-10-18 10:25:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         raidbots翻译
 // @namespace    http://tampermonkey.net/
-// @version      v1.0
+// @version      v1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.raidbots.com/simbot*
@@ -16,37 +16,14 @@
     'use strict';
     // 自定义翻译词典
     let translationDictionary = {}
-    GM.xmlHttpRequest({
-        method: "GET",
-        url: "https://raw.githubusercontent.com/levilus/simc_chinese_script/refs/heads/main/trans.json",// 远程 JSON 文件的 URL
-        onload: function(response) {
-            if (response.status === 200) {
-                try {
-                    // 解析 JSON 数据
-                    translationDictionary = JSON.parse(response.responseText);
-                    console.log('Fetched and parsed JSON data:', translationDictionary);
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
-                }
-            } else {
-                console.error('Failed to fetch JSON, status:', response.status);
-            }
-        },
-        onerror: function(error) {
-            console.error('Request error:', error);
-        }
-    });
     // 翻译文本函数：根据自定义词典替换文本
     function translateLine(text) {
         let translatedText = text.trim()
-        for (const [original, translated] of Object.entries(translationDictionary)) {
-            if(translatedText.toLowerCase() == original) {
-                console.log('translated: ' + text)
-                translatedText = translated;
-                break;
-            }
+        let transed = translationDictionary[translatedText.toLowerCase()]
+        if(transed === undefined) {
+            return translatedText;
         }
-        return translatedText
+        return transed
     }
     function translateUnit(str,before,after){
         let index = str.indexOf('('+before+')')
@@ -112,33 +89,50 @@
             translateNode(element) // 递归翻译元素和子节点
         });
     }
-    const observer = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            if (mutation.type === 'childList') {
-                // 处理新增的节点
-                mutation.addedNodes.forEach(node => {
-                    //if (node.nodeType === Node.ELEMENT_NODE) {
-                    //  const elements = node.querySelectorAll('*');
-                    //elements.forEach(el => translateNode(el));
-                    //   }
-                    translateNode(node);
-                });
-            } else if (mutation.type === 'characterData') {
-                // 处理文本内容变化
-                const parentElement = mutation.target.parentElement;
-                if (parentElement) {
-                    translateNode(parentElement);
-                }
-            }
-        })
-    })
-    const config = {childList:true, subtree:true, characterData:true}
-    observer.observe(document.body, config)
     window.addEventListener('load', function(){
-        console.log('trans loaded')
-        translatePage()
+        console.log(111)
+        GM.xmlHttpRequest({
+            method: "GET",
+            url: "https://raw.githubusercontent.com/levilus/simc_chinese_script/refs/heads/main/trans.json",// 远程 JSON 文件的 URL
+            onload: function(response) {
+                if (response.status === 200) {
+                    try {
+                        // 解析 JSON 数据
+                        translationDictionary = JSON.parse(response.responseText);
+                        translatePage()
+                        const observer = new MutationObserver(mutations => {
+                            mutations.forEach(mutation => {
+                                if (mutation.type === 'childList') {
+                                    // 处理新增的节点
+                                    mutation.addedNodes.forEach(node => {
+                                        //if (node.nodeType === Node.ELEMENT_NODE) {
+                                        //  const elements = node.querySelectorAll('*');
+                                        //elements.forEach(el => translateNode(el));
+                                        //   }
+                                        translateNode(node);
+                                    });
+                                } else if (mutation.type === 'characterData') {
+                                    // 处理文本内容变化
+                                    const parentElement = mutation.target.parentElement;
+                                    if (parentElement) {
+                                        translateNode(parentElement);
+                                    }
+                                }
+                            })
+                        })
+                        const config = {childList:true, subtree:true, characterData:true}
+                        observer.observe(document.body, config)
+                    } catch (error) {
+                        console.error('Error parsing JSON:', error);
+                    }
+                } else {
+                    console.error('Failed to fetch JSON, status:', response.status);
+                }
+            },
+            onerror: function(error) {
+                console.error('Request error:', error);
+            }
+        });
     })
-    //const s1 = document.getElementById('AdvancedSimOptions-fightStyle')
-    //s1.addEventListener('change', function(event){
-    //})
+
 })();
